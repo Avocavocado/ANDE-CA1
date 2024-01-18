59e3ca0ecba92cc7e7b43e7ea4413e93fe160a9f
jasontok123 <JTZS20.22@ichat.sp.edu.sg>
2024-01-18 15:37:33 +0800

    Added getpartyUserDetails function

--- app/src/main/java/com/example/ande_munch/methods/Callback.java
+++ app/src/main/java/com/example/ande_munch/methods/Callback.java
@@ -1,5 +1,12 @@
 package com.example.ande_munch.methods;
 
+import java.util.List;
+import java.util.Map;
+
 public interface Callback {
     void onUserChecked(boolean userExists);
+
+    void onUserDataFetched(List<Map<String, Object>> usersList);
+
+    void onFailure(Exception e);
 }
