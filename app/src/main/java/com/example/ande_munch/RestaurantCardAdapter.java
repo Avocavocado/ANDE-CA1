369d9d59f9d0c87a12b3239004df48a98a15461a
dillan <waltonmcbobby@gmail.com>
2024-01-21 12:18:29 +0800

    EXPLORE RESTAURANTS + FILTER ACTIVTY: Styling done C:

--- app/src/main/java/com/example/ande_munch/RestaurantCardAdapter.java
+++ app/src/main/java/com/example/ande_munch/RestaurantCardAdapter.java
@@ -2,6 +2,8 @@ package com.example.ande_munch;
 
 import android.content.Context;
 import android.graphics.Color;
+import android.text.SpannableString;
+import android.text.style.ForegroundColorSpan;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -9,13 +11,16 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
-
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.ande_munch.classes.Restaurant;
 import com.squareup.picasso.Picasso;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
+import java.util.Map;
 
 public class RestaurantCardAdapter extends RecyclerView.Adapter<RestaurantCardAdapter.ViewHolder> {
 
@@ -30,10 +35,10 @@ public class RestaurantCardAdapter extends RecyclerView.Adapter<RestaurantCardAd
     public static class ViewHolder extends RecyclerView.ViewHolder {
         private final TextView RestaurantName;
         private final TextView RestaurantDesc;
+        private final TextView OpenInfo;
         private final ImageView RestaurantImage;
+        private final TextView Rating;
         private final LinearLayout Cuisines;
-        private double avgPrice;
-        private double avgRating;
         private Restaurant restaurant;
 
         public ViewHolder(View view) {
@@ -43,7 +48,9 @@ public class RestaurantCardAdapter extends RecyclerView.Adapter<RestaurantCardAd
             RestaurantName = (TextView) view.findViewById(R.id.RestaurantName);
             RestaurantDesc = (TextView) view.findViewById(R.id.RestaurantDesc);
             RestaurantImage = (ImageView) view.findViewById(R.id.RestaurantImage);
+            OpenInfo = (TextView) view.findViewById(R.id.OpenInfo);
             Cuisines = (LinearLayout) view.findViewById(R.id.Cuisines);
+            Rating = (TextView) view.findViewById(R.id.restaurantRating);
 
             itemView.setOnClickListener(new View.OnClickListener() {
                 @Override
@@ -60,6 +67,12 @@ public class RestaurantCardAdapter extends RecyclerView.Adapter<RestaurantCardAd
         }
         public TextView getDesc() {
             return RestaurantDesc;
+        }
+        public TextView getOpenInfo() {
+            return OpenInfo;
+        }
+        public TextView getRating() {
+            return Rating;
         }
         public ImageView getImage() { return RestaurantImage; }
         public LinearLayout getCuisines() { return Cuisines; }
@@ -91,12 +104,19 @@ public class RestaurantCardAdapter extends RecyclerView.Adapter<RestaurantCardAd
     @Override
     public void onBindViewHolder(ViewHolder viewHolder, final int position) {
         Restaurant item = localDataSet.get(position);
-        Log.i("GPS", "ResCard: " + item.distance);
-        viewHolder.getName().setText(item.data.getId() + " " + item.distance + "km");
-        viewHolder.getDesc().setText(item.data.getString("Desc"));
-        Picasso.get().load(item.data.getString("RestaurantImage")).into(viewHolder.getImage());
         viewHolder.setData(item);
 
+        viewHolder.getName().setText(item.data.getId() );
+        viewHolder.getDesc().setText(item.data.getString("Desc"));
+        //viewHolder.getAddress().setText("• " + item.data.getString("Address") + " (" + (item.distance) + "km)");
+
+        //Average Rating
+        SpannableString avgRating = new SpannableString(item.avgRating + "★");
+        avgRating.setSpan(new ForegroundColorSpan(Color.rgb(255,199,44)), 3, 4, 0);
+        viewHolder.getRating().setText(avgRating);
+        Picasso.get().load(item.data.getString("RestaurantImage")).into(viewHolder.getImage());
+
+        //Cuisine Boxes
         viewHolder.getCuisines().removeAllViews();
         for (String cuisine: (List<String>) item.data.get("Cuisine")) {
             TextView textView = new TextView(context);
@@ -116,6 +136,33 @@ public class RestaurantCardAdapter extends RecyclerView.Adapter<RestaurantCardAd
             viewHolder.getCuisines().addView(textView);
 
         }
+
+        //Opening Hours
+        int dayOfWeek = LocalDate.now().getDayOfWeek().getValue() - 1;
+        List<Object> openingHours = (List<Object>) item.data.get("OpeningHours");
+        Map<String,String> todayOH = (Map<String, String>) openingHours.get(dayOfWeek);
+        LocalTime now = LocalTime.now();
+        LocalTime openingTime = LocalTime.parse(todayOH.get("Open"), DateTimeFormatter.ofPattern("H:mm"));
+        LocalTime closingTime = LocalTime.parse(todayOH.get("Close"), DateTimeFormatter.ofPattern("H:mm"));
+
+        boolean hasOpened = !now.isBefore(openingTime);
+        boolean hasClosed = !now.isBefore(closingTime);
+
+        if (hasOpened && !hasClosed) {
+            viewHolder.getOpenInfo().setBackgroundColor(Color.parseColor("#00FF66"));
+            viewHolder.getOpenInfo().setText("Open til " + closingTime.format(DateTimeFormatter.ofPattern("hh:mm a")));
+        }
+        else if (!hasOpened && !hasClosed){
+            viewHolder.getOpenInfo().setBackgroundColor(Color.parseColor("#30303A"));
+            viewHolder.getOpenInfo().setText("Opens at " + openingTime.format(DateTimeFormatter.ofPattern("hh:mm a")));
+        }
+        else {
+            viewHolder.getOpenInfo().setBackgroundColor(Color.parseColor("#30303A"));
+            Map<String,String> tomorrowOH = (Map<String, String>) openingHours.get(dayOfWeek != 6 ? dayOfWeek+1 : 0);
+            LocalTime tomorrowOpeningTime = LocalTime.parse(tomorrowOH.get("Open"));
+            viewHolder.getOpenInfo().setText("Closed til " + tomorrowOpeningTime.format(DateTimeFormatter.ofPattern("hh:mm a")));
+        }
+        //Log.i("OpenInfo", openingTime + "-" + closingTime);
     }
 
     public void updateData(List<Restaurant> newDataSet) {
